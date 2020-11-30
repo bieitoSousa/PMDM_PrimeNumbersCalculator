0dd10cb4496d7fd67d352d6c6c18ec4597f3b883
bieito.studies <bieito.studies@gmail.com>
2020-11-30 22:07:12 +0100

    second commit

--- app/src/main/java/com/bieitosousa/primenumberscalculator/MainActivity.java
+++ app/src/main/java/com/bieitosousa/primenumberscalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bieitosousa.primenumberscalculator;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 
@@ -10,40 +11,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
+import android.util.Log;
+
+import android.view.Gravity;
 import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-/*   =            =      Texto      =
-            [https://developer.android.com/guide/topics/resources/string-resource?hl=es#java]
-                #   En Java: R.string.string_name
-                        -----------------------------------------------
-                        String string = getString(R.string.hello);
-                        -----------------------------------------------
-                #   En XML:@string/string_name
-     =            =     Color     =
-               deprecate --> [https://developer.android.com/guide/topics/resources/more-resources#Color]
-               actual --> [https://stackoverflow.com/questions/31590714/getcolorint-id-deprecated-on-android-6-0-marshmallow-api-23]
-                #   En Java: R.color.color_name
-                     --------------------------------------------------
-                        Resources res = getResources();
-                        //  int color = res.getColor(R.color.opaque_red);
-                        // getColor deprecate [int color = res.(R.color.opaque_red);]
+
+    private static final String TAG = "MyActivity";
+    private static final String TAG_onCreate = TAG +" : onCreate";
+    private static final String TAG_onCreate_button_onClick = TAG_onCreate+ " : button_action_calculate : onClick";
 
 
-                       ContextCompat.getColor(this, R.color.orange_primary)
-                    ----------------------------------------------------
-                #  En XML: @[package:]color/color_name
- */
+    /*   =            =      Texto      =
+                [https://developer.android.com/guide/topics/resources/string-resource?hl=es#java]
+                    #   En Java: R.string.string_name
+                            -----------------------------------------------
+                            String string = getString(R.string.hello);
+                            -----------------------------------------------
+                    #   En XML:@string/string_name
+         =            =     Color     =
+                   deprecate --> [https://developer.android.com/guide/topics/resources/more-resources#Color]
+                   actual --> [https://stackoverflow.com/questions/31590714/getcolorint-id-deprecated-on-android-6-0-marshmallow-api-23]
+                    #   En Java: R.color.color_name
+                         --------------------------------------------------
+                            Resources res = getResources();
+                            //  int color = res.getColor(R.color.opaque_red);
+                            // getColor deprecate [int color = res.(R.color.opaque_red);]
+
+
+                           ContextCompat.getColor(this, R.color.orange_primary)
+                        ----------------------------------------------------
+                    #  En XML: @[package:]color/color_name
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.i(TAG_onCreate, " - START -");
         try {
-            Resources res = getResources(); // to access res/ to  take color and other
+            Resources res = getResources(); // to access res/ to  take color and other parameters
             // init main activity
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
@@ -53,53 +65,57 @@ public class MainActivity extends AppCompatActivity {
             final TextView textView_header = (TextView) findViewById(R.id.textView_header);
             final TextView textView_description = (TextView) findViewById(R.id.textView_description);
             final EditText editText_int_Input = (EditText) findViewById(R.id.editText_int_Input);
+            final TextView textView_Label_position = (TextView) findViewById(R.id.textView_Label_position);
             final Button button_action_calculate = (Button) findViewById(R.id.button_action_calculate);
             final TextView textView_request_action = (TextView) findViewById(R.id.textView_request_action);
+
             // set atributes for the inputs
+           //header
+            textView_header.setText(getString(R.string.header));
+            textView_header.setTextColor(ContextCompat.getColor(this, R.color.orange_primary));
+            //description
+            textView_description.setText(getString(R.string.description));
+            //button
             button_action_calculate.setText(getString(R.string.button_action_calculate_text));
             button_action_calculate.setTextColor(ContextCompat.getColor(this, R.color.white));
             button_action_calculate.setBackgroundColor(ContextCompat.getColor(this, R.color.orange_primary));
-            textView_header.setText(getString(R.string.header));
-            textView_header.setTextColor(ContextCompat.getColor(this, R.color.orange_primary));
-            textView_description.setText(getString(R.string.description));
-            // take atributes for the inputs
-            String inputNumberString =  editText_int_Input.getText().toString(); // input in type String
-            int inputNumberInt = Integer.parseInt(inputNumberString);
-            final String finalRequest = preRequest + inputNumberInt + postRequest ;
+            // input label
+            textView_Label_position.setText(getString(R.string.labelPosition));
+            textView_Label_position.setTextColor(ContextCompat.getColor(this, R.color.orange_primary));
+
+
             // Events --> On
-            button_action_calculate.setOnClickListener( new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // calculate outputs
-                   int numberRequest = PrimeCalculator.numberOfPrimoInt(inputNumberInt);
-                    // request ouputs
-                    textView_request_action.setText(finalRequest + numberRequest);
-                }
-            });
-        }catch(Exception e){
-            System.err.println(e.getMessage());
+            button_action_calculate.setOnClickListener(
+                    new View.OnClickListener()  {
+                        @Override
+                        public void onClick(View v) throws NumberFormatException {
+                            try {
+                            // take atributes for the inputs
+                            final String inputNumberString = editText_int_Input.getText().toString(); // input in type String
+                            int inputNumberInt = Integer.parseInt(inputNumberString);
+                            final String finalRequest = preRequest + " [ " + inputNumberInt + " ] " + postRequest + "=> [ ";
+                            // calculate outputs
+                            Toast toast1 =
+                                    Toast.makeText(getApplicationContext(),
+                                            "Toast por defecto", Toast.LENGTH_SHORT);
+                            int numberRequest = PrimeCalculator.numberOfPrimoInt(inputNumberInt);
+                            // request ouputs
+                            textView_request_action.setText(finalRequest + numberRequest + " ]");
+                            } catch (NumberFormatException nfe) {
+                               Toast toast = Toast.makeText(MainActivity.this ,getString(R.string.errorEntero), Toast.LENGTH_LONG);
+                                toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+                                toast.show();
+                                Log.d(TAG_onCreate_button_onClick, "NumberFormatException "+nfe.getMessage());
+                            } catch (Exception e) {
+                                Toast.makeText(MainActivity.this ,e.getMessage(), Toast.LENGTH_LONG).show();
+                                Log.d(TAG_onCreate_button_onClick,"Exception"+ e.getMessage());
+                            }
+                        }
+                    });
+        } catch (Exception e) {
+            Log.i(TAG_onCreate,"MainActivity"+ e.getMessage());
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/bieitosousa/primenumberscalculator/PrimeCalculator.java
+++ app/src/main/java/com/bieitosousa/primenumberscalculator/PrimeCalculator.java
@@ -4,13 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeCalculator {
-
-    public static boolean esPrimo(int numero){
+/*
+ un número primo es un número natural mayor que 1
+ que tiene únicamente dos divisores positivos distintos:
+  él mismo y el 1
+ */
+    private static boolean esPrimo(int numero){
+        // si el numero es 1 ó 0 es primo
+        if (numero  == 0 || numero == 1){
+            return true;
+        }
+        // comenzamso en el numero posterior al 1 --> 2
         int contador = 2;
         boolean primo=true;
-        while ((primo) && (contador!=numero)){
-            if (numero % contador == 0)
+        // si el numero divido entre el rango [2 -> numero -1]  no tiene division entera es primo.
+        while ( contador!=numero){
+            if (numero % contador == 0) {
                 primo = false;
+            }
             contador++;
         }
         return primo;
@@ -40,9 +51,13 @@ public class PrimeCalculator {
         return null;
     }
     public static int numberOfPrimoInt(int number){
+        int res = -1;
         try{
+
             List<Integer> nPrimoList= new ArrayList<Integer>();
-            int startIndex = 0;
+            // entendiendo que el 0 no es un numero natural
+            // comenzamos en el 1.
+            int startIndex = 1;
             int greater = 1000000;
             int less = 0;
             if (number < less && number > greater){
@@ -51,16 +66,18 @@ public class PrimeCalculator {
                                 + greater
                                 + " and less than" +less );
             }
-            while (nPrimoList.size() == number){
+            while (nPrimoList.size() != number){
                 if (esPrimo(startIndex)) {
                     nPrimoList.add(startIndex);
                 }
                 startIndex++;
             }
-            return startIndex;
+            // devolvemos el ultimo numero primo (Posicion comienza en 0 , y size en 1)
+            res = nPrimoList.get(nPrimoList.size()-1);
+            return res;
         }catch(Exception e ){
             System.err.println(e.getMessage());
         }
-        return -1;
+        return res;
     }
 }
